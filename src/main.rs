7de98640d9ce7b25d7aee59dbae2e9ef35e0b7a0
(Tim) Efthimis Kritikos <git-27@mail.efthimiskritikos.com>
2026-03-11 11:30:53 +0000

    Added code to create and read the config file

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,58 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
+use std::path::PathBuf;
+use std::io;
+use std::io::Write;
+use serde::{Deserialize, Serialize};
+use std::process;
+use std::fs::File;
+use home::home_dir;
+use anyhow::{Result};
+
+#[derive(Deserialize, Serialize)]
+struct MainConfig {
+    data_type: String,
+    data_structure_version: String,
+    allow_device_list: Vec<String>,
+    ignore_device_list: Vec<String>,
+}
+
+fn parse_config_file(config_file_path:PathBuf) -> Result<MainConfig> {
+    if ! config_file_path.exists(){
+        print!("Config file doesn't exist. Create an empty one? (y/n): ");
+        let _ = io::stdout().flush();
+        let mut buffer = String::new();
+        io::stdin().read_line(&mut buffer)?;
+        if buffer != "y\n" {
+            process::exit(0);
+        }
+        let new_config = MainConfig{
+            data_type:"ingest_and_snapshot_config".to_string(),
+            data_structure_version:"v0.0".to_string(),
+            allow_device_list: [].to_vec(),
+            ignore_device_list: [].to_vec(),
+        };
+
+        let mut config_file = File::create(config_file_path)?;
+        let  _ = config_file.write_all( serde_json::to_string(&new_config)?.as_bytes());
+        Ok(new_config)
+    }else{
+        match std::fs::read_to_string(&config_file_path) {
+            Ok(data) => Ok(serde_json::from_str(&data)?),
+            Err(e) => {
+                eprintln!("Failed to read config file {:?}: {}", config_file_path, e);
+                Err(e.into())
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("well, here we go");
+    let home = home_dir().expect("Could not determine home directory");
+    let config_file_path = home.join("ingest_and_snapshot_config.json");
+
+    let config = parse_config_file(config_file_path).unwrap();
+
+    println!("allow {:?}\ndeny {:?}",config.allow_device_list ,config.ignore_device_list);
 }
